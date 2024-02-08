e267ecf80e7c40ef8e20fb2512ac55e90df214b7
Momen <Moumen.Guinoubi@wimbeetech.com>
2024-02-08 13:59:50 +0100

    Task nine

--- task_9.bash
+++ task_9.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-while IFS= read -r l ; do
- echo "$l"
+while IFS=":" read USERNAME GID HOME_DIRECTORY PASSWORD UID Command; do
+ echo " The user $USERNAME is part of the $GID gang, lives in $HOME_DIRECTORY and rides $Command. USER ID's place is protected by the passcode $PASSWORD, more info about the user here:$UID"
 done < /etc/passwd
